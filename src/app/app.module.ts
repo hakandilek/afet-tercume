87a46988889609e735bf5ebcf4a502533dab3496
Fikret <fikret.buyukisik2@bam.com>
2023-02-11 22:47:15 +0100

    feat(pwa): offline caching of API call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWithDelay:5000'
     }),
   ],
   providers: [],
