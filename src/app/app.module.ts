4480a088c96a55910177b457f8a09a4fb8b5133b
Hakan Dilek <hakandilek@gmail.com>
2023-02-12 00:01:48 +0100

    chore: organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
 import { HeaderComponent } from './header/header.component';
-import { SharedModule } from './shared/shared.module';
-import { TermsModule } from './terms/terms.module';
-import { EffectsModule } from '@ngrx/effects';
-import { TermsEffects } from './terms/reducer/terms.effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { metaReducers, reducers } from './reducers';
-import { environment } from 'src/environments/environment';
+import { SharedModule } from './shared/shared.module';
+import { TermsEffects } from './terms/reducer/terms.effects';
 import { TermListComponent } from './terms/term-list.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { TermsModule } from './terms/terms.module';
 
 @NgModule({
   declarations: [
