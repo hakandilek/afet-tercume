1cc9b177a5cbc55514579c8c4a9e165b69b6ffa5
Hakan Dilek <hakandilek@gmail.com>
2023-02-11 14:49:10 +0100

    setup angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { StoreModule } from '@ngrx/store';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
