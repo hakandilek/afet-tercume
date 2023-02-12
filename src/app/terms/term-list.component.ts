e0fd3995ab053526d665b4520749aada45c9d51b
Fikret <fikret.buyukisik2@bam.com>
2023-02-12 03:44:49 +0100

    feat(styling): list styling, sorting, initial letter extraction, search field styling

--- src/app/terms/term-list.component.ts
+++ src/app/terms/term-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { State } from '../reducers';
 import { selectAll } from './reducer';
 import { loadTerms } from './reducer/terms.actions';
@@ -15,14 +15,36 @@ import { Term } from './term';
 export class TermListComponent implements OnInit {
   searchTerm = ''
   terms$: Observable<Term[]>;
-
   constructor(
     private store: Store<State>,
   ) {
-    this.terms$ = this.store.select(selectAllTerms)
+    this.terms$ = this.store.select(selectAllTerms).pipe(map((terms) => {
+      const sortedTerms = terms.slice().sort((a, b) => {
+        const t1 = a.translations.get('Türkçe')?.toLocaleLowerCase();
+        const t2 = b.translations.get('Türkçe')?.toLocaleLowerCase();
+        return this.compareNormalized(t1, t2, 'tr');
+      });
+      return sortedTerms;
+    }));
   }
 
   ngOnInit(): void {
     this.store.dispatch(loadTerms());
   }
+
+  private compareNormalized(a: any, b: any, locale: string): CompareResult {
+    if (typeof a === 'undefined') {
+      return 1;
+    }
+    if (b === undefined || b === null) {
+      return -1;
+    }
+    return a.localeCompare(b, locale, {
+      ignorePunctuation: true,
+      sensitivity: 'variant',
+      numeric: true,
+    }) as CompareResult;
+  }
 }
+
+type CompareResult = -1 | 0 | 1;
