58e50ef90cb4db1eb4367bc2e84c372ef22db277
Hakan Dilek <hakandilek@gmail.com>
2023-02-18 01:11:03 +0100

    test: run tests with headless chrome

---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/afet-terim'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text' },
+        { type: 'text-summary' },
+        { type: 'lcov', subdir: 'report-lcov' },
+      ]
+    },
+    reporters: [
+      'progress',
+      'kjhtml',
+      'spec',
+      'coverage'
+    ],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
