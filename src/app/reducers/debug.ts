15617595ef3bcfb690ae8dac1b3644a7cb378675
Hakan Dilek <hakandilek@gmail.com>
2025-04-19 20:56:52 +0200

    fix(linting): fix or ignore linting errors

--- src/app/reducers/debug.ts
+++ src/app/reducers/debug.ts
@@ -1,7 +1,7 @@
 import { ActionReducer } from '@ngrx/store';
 
 // console.log all actions
-export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+export function debug<T>(reducer: ActionReducer<T>): ActionReducer<T> {
   return (state, action) => {
     // console.log(`state: ${JSON.stringify(state)}, action: ${JSON.stringify(action.type)}`);
     return reducer(state, action);
--- src/app/terms/highlight.pipe.ts
+++ src/app/terms/highlight.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HighlightPipe implements PipeTransform {
 
-  transform(value: any, ...args: any): unknown {
+  transform(value: string, args?: string): unknown {
     if (!args) { return value; }
     const re = new RegExp(args, 'gi');
     return value.replace(re, "<mark>$&</mark>");
